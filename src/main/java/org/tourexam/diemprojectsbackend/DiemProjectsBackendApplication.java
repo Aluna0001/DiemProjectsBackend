999235286879114ac04f9c34e95b5d35ac4c05a2
Aluna0001 <lusz0001@stud.kea.dk>
2025-05-02 14:43:52 +0200

    Initial commit

---
+++ src/main/java/org/tourexam/diemprojectsbackend/DiemProjectsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tourexam.diemprojectsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiemProjectsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiemProjectsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tourexam/diemprojectsbackend/DiemProjectsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tourexam.diemprojectsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiemProjectsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
